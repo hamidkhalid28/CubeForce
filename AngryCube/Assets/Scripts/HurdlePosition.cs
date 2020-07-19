5f8dfb693c4e6183ac7971d3ef13120211fb910b
Hamid Khalid <hamid@happslab.com>
2020-07-19 15:40:40 +0200

    Added gitignore file for unity Project contains movement and hurdles setup.

---
+++ AngryCube/Assets/Scripts/HurdlePosition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurdlePosition : MonoBehaviour
+{
+    Transform[] child;
+    // Start is called before the first frame update
+    void Start()
+    {
+        child = new Transform[2];
+        child[0] = transform.GetChild(0);
+        child[1] = transform.GetChild(1);
+
+        randomizePosition();
+
+    }
+
+    void randomizePosition()
+    {
+        child[0].transform.position = new Vector3(Random.Range(0.5f, 2.5f), transform.position.y, transform.position.z);
+        child[1].transform.position = new Vector3(child[0].transform.position.x - 3, transform.position.y, transform.position.z);
+    }
+
+}
